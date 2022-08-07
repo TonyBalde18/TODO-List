baf5d4cc89b5fc59c7b0a8e23096efdd66fb0c3e
Tony Baldé <antonio.balde@ubi.pt>
2022-08-07 09:45:29 +0100

    added filter feature to completed tasks

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //EVENT LISTENERS
 todoButton.addEventListener("click", addTodo);
-todoList.addEventListener("click", deleteCheck);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
 
 //FUNCTIONS
 function addTodo(event) {
@@ -24,10 +26,10 @@ function addTodo(event) {
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
   //Delete button
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = '<i class="fas fa-trash"></i>'; //add tag to the button
-  deleteButton.classList.add("delete-btn");
-  todoDiv.appendChild(deleteButton);
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>'; //add tag to the button
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
   //Append to list
   todoList.appendChild(todoDiv); //ln 13
   //clear the input area after clicking on add btn
@@ -35,11 +37,12 @@ function addTodo(event) {
 }
 //Delete a task
 //apply fall and fade animation first and then remove the task from the list
-function deleteCheck(e) {
+function deleteTodo(e) {
   const item = e.target;
-  if (item.classList[0] === "delete-btn") {
+  if (item.classList[0] === "trash-btn") {
     const todo = item.parentElement;
     todo.classList.add("fall"); //Animation
+    
     todo.addEventListener("transitionend", function () {
       //will execute the function after animation finishes
       todo.remove();
@@ -51,4 +54,32 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
   }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    /*retrieve the value defined on the html file*/
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break; /*supposed to show all the tasks, since it shows by default -> skip */
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          /*check if the task is completed and show it*/
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")) {
+          /*show every task that is not completed*/
+          todo.styles.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
